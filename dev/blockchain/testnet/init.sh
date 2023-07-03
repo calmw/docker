fb94b1c622c012856ad4a7cafed77d29ebd19682
calmw <calm.wang@hotmail.com>
2023-07-03 18:51:45 +0800

    docker 搭建geth私链

--- dev/blockchain/testnet/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/ethereum/geth --datadir "/project/node_data" --networkid 7000 --http.port 7545 --identity "TestNet" init /ethereum/genesis.json &&
-/ethereum/geth --datadir "/project/node_data" account new
-
--- dev/blockchain/testnet/start.sh
+++ dev/blockchain/testnet/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-/ethereum/geth --allow-insecure-unlock  --unlock=0 --nodiscover --password  "/ethereum/password"  --networkid  7000  --datadir  "/project/node_data"  --http --http.api "admin,debug,web3,eth,txpool,personal,ethash,miner,net" --http.corsdomain="*" --http.port=7545 --http.addr="0.0.0.0"  --ws --ws.addr "0.0.0.0" --ws.port=7546 --ws.origins "*" --ws.api "admin,debug,web3,eth,txpool,personal,ethash,miner,net" --syncmode full console
+/ethereum/geth --nodiscover --identity "TestNet" --networkid 7000 --datadir "/project/node_data" --allow-insecure-unlock --gcmode=archive --mine --miner.threads=1 --miner.etherbase=0xD5f92Fd92F8c7f9391513E3019D9441aAf5b2D9E --http --http.addr 0.0.0.0 --http.port 7545 --http.corsdomain "*" --ws --ws.addr 0.0.0.0 --ws.port 7546 --ws.api "admin,debug,web3,eth,txpool,personal,ethash,miner,net" --http.api "admin,debug,web3,eth,txpool,personal,ethash,miner,net" --ws.origins "*" >> /ethereum/geth.log 2>&1
+
+
+#/ethereum/geth --allow-insecure-unlock  --unlock=0 --nodiscover --password  "/ethereum/password"  --networkid  7000  --datadir  "/project/node_data"  --http --http.api "admin,debug,web3,eth,txpool,personal,ethash,miner,net" --http.corsdomain="*" --http.port=7545 --http.addr="0.0.0.0"  --ws --ws.addr "0.0.0.0" --ws.port=7546 --ws.origins "*" --ws.api "admin,debug,web3,eth,txpool,personal,ethash,miner,net" --syncmode full console
